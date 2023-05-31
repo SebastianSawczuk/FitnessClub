4c88e27ae0f22f79cca21db0f0daa47a8a99e832
Sebastian Sawczuk <s.sawczuk99@gmail.com>
2023-05-31 11:36:21 +0200

    clean

--- FitnessClub/Models/User.cs
+++ FitnessClub/Models/User.cs
@@ -2,5 +2,25 @@
 {
     public class User
     {
+        public User()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string E_mail { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public User(int id, string name, string lastName, string e_mail, string login, string password)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            E_mail = e_mail;
+            Login = login;
+            Password = password;
+        }
     }
 }
